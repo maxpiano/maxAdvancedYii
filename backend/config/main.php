d0c4360849be314e1f47bfe38ec59cd471904f9c
Max B <maxbilov@gmail.com>
2020-06-30 13:25:53 +0200

    installed bootstrap4

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,6 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'language' => 'de=DE',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
